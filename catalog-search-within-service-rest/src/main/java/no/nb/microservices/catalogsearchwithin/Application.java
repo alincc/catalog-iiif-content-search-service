3da24b6f1f370c49525e4f3625ee89a4a5eb14f9
ronnymikalsen <ronny.mikalsen@nb.no>
2015-11-12 12:16:02 +0100

    init

---
+++ catalog-search-within-service-rest/src/main/java/no/nb/microservices/catalogsearchwithin/Application.java
@@ -0,0 +1,27 @@
+package no.nb.microservices.catalogsearchwithin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
+
+import no.nb.htrace.annotation.EnableTracing;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableOAuth2Resource
+@RefreshScope
+@EnableTracing
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ catalog-search-within-service-rest/src/main/java/no/nb/microservices/catalogsearchwithin/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package no.nb.microservices.catalogsearchwithin.config;
+
+import no.nb.commons.web.sso.feign.SsoFeignInterceptor;
+import no.nb.commons.web.xforwarded.feign.XForwardedParamsFeignInterceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@Import({XForwardedParamsFeignInterceptor.class, SsoFeignInterceptor.class})
+public class SecurityConfig extends ResourceServerConfigurerAdapter {
+
+    
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.requestMatchers()
+                .antMatchers("/**")
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated();
+    }
+}
