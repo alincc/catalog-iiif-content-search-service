8739876405047cb5a67f01459da23687ae3fc99d
ronnymikalsen <ronny.mikalsen@nb.no>
2015-11-18 15:22:55 +0100

    BT-3561 Search BT-3564 Integration test

---
+++ catalog-iiif-content-search-service-it/src/test/java/no/nb/microservices/catalogcontentsearch/SearchControllerIT.java
@@ -0,0 +1,112 @@
+package no.nb.microservices.catalogcontentsearch;
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.loadbalancer.BaseLoadBalancer;
+import com.netflix.loadbalancer.ILoadBalancer;
+import com.netflix.loadbalancer.Server;
+import com.squareup.okhttp.mockwebserver.Dispatcher;
+import com.squareup.okhttp.mockwebserver.MockResponse;
+import com.squareup.okhttp.mockwebserver.MockWebServer;
+import com.squareup.okhttp.mockwebserver.RecordedRequest;
+
+import no.nb.commons.web.util.UserUtils;
+import no.nb.microservices.catalogcontentsearch.rest.model.AnnotationList;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {Application.class, RibbonClientConfiguration.class})
+@WebIntegrationTest("server.port:0")
+public class SearchControllerIT {
+
+    @Value("${local.server.port}")
+    int port;
+
+    @Autowired
+    ILoadBalancer lb;
+
+    RestTemplate template = new TestRestTemplate();
+    
+    MockWebServer server;
+    
+    @Before
+    public void setup() throws Exception {
+
+        server = new MockWebServer();
+        final Dispatcher dispatcher = new Dispatcher() {
+            String searchid1Mock = IOUtils.toString(this.getClass().getResourceAsStream("id1.json"));
+
+            @Override
+            public MockResponse dispatch(RecordedRequest request) throws InterruptedException {
+                if (request.getPath().startsWith("/id1/search?q=test")) {
+                    return new MockResponse().setBody(searchid1Mock).setHeader("Content-Type", "application/json; charset=utf-8");
+                }
+                return new MockResponse().setResponseCode(404);
+            }
+
+        };
+        server.setDispatcher(dispatcher);
+        server.start();
+
+        BaseLoadBalancer blb = (BaseLoadBalancer) lb;
+        blb.setServersList(Arrays.asList(new Server(server.getHostName(), server.getPort())));
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        server.shutdown();
+    }    
+    
+    @Test
+    public void testSearch() {
+        HttpHeaders headers = createDefaultHeaders();
+        
+        ResponseEntity<AnnotationList> response = new TestRestTemplate().exchange(
+                "http://localhost:" + port + "/catalog/contentsearch/id1/search?q=test", HttpMethod.GET,
+                new HttpEntity<Void>(headers), AnnotationList.class);
+        AnnotationList annotationList = response.getBody();
+        
+        assertTrue("Repsonse code should be successful", response.getStatusCode().is2xxSuccessful());
+        assertNotNull("AnnotationList should not be null", annotationList);
+        assertEquals("Should hava a context", "http://iiif.io/api/search/0/context.json", annotationList.getContext());
+        assertEquals("Should have a type", "sc:AnnotationList", annotationList.getType());
+
+    }
+    
+    private HttpHeaders createDefaultHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(UserUtils.SSO_HEADER, "token");
+        headers.add(UserUtils.REAL_IP_HEADER, "123.45.100.1");
+        return headers;
+    }    
+
+}
+
+@Configuration
+class RibbonClientConfiguration {
+
+    @Bean
+    public ILoadBalancer ribbonLoadBalancer() {
+        return new BaseLoadBalancer();
+    }
+}
